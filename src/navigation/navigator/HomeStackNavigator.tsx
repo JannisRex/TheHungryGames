6e8ac7be76946fca4d5757bebff81bfb00382c75
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:05:43 +0100

    [added] react-navigation again, needd for createAppContainer
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/navigation/navigator/HomeStackNavigator.tsx
@@ -0,0 +1,20 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator} from 'react-navigation-stack'
+import { Component } from 'react'
+import HomeScreen from '../../screens/home/main'
+
+const HomeStackNavigation = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        header: null
+      }
+    }
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
+
+export default createAppContainer(HomeStackNavigation)

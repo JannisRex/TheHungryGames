73c01bb5b08701cbf505a292d2bd4eb7754ba2e6
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:09:22 +0100

    [fixed] spelling mistake
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,3 +1,3 @@
 export { default as AppTabNavigator } from './navigator/AppTabNavigator'
 export { default as HomeStackNavigator } from './navigator/HomeStackNavigator'
-export { default as PreloadSwitchNavigatior } from './navigator/PreloadSwitchNavigatior'
+export { default as PreloadSwitchNavigator } from './navigator/PreloadSwitchNavigator'

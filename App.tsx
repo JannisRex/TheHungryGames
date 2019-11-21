c5a61e53549de1642f8a51f9907f1c00433d866f
Jannis Rex <rex.jannis@web.de>
2019-11-21 11:57:39 +0100

    [added] correct typing for render() ITS NOT NODE :))
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
+
 class App extends Component {
-  render(): void {
+  render(): JSX.Element {
     return (
       <View>
         <Text>a</Text>

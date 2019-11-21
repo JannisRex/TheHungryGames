34509d47f3ee3e5349b33b4b6da90cebe31c1a19
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:58:18 +0100

    [added] semi structured logic => loads Fonts & Logo && [TODO] handle AsyncStorage 4 real
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/screens/initialLoad/main.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import React from 'react'
+import { AsyncStorage } from 'react-native'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import { Asset } from 'expo-asset'
+
+// here we fetch AsyncStorage and check,
+// if we stored something earlier, then
+// we save it to state and pass to homeScreen
+const _getPreferences = async (): void => {
+  try {
+    await AsyncStorage.getItem('@settings').then((result: {}) => {
+      if (typeof result === typeof Object) {
+        console.log('initialLoad->getPreferences()')
+        console.log(result)
+      }
+    })
+  } catch (e) {
+    console.log('error getting theme-')
+  }
+}
+
+// here we execute everything, thats about loading
+// e.g. => Getting Fonts, Images, fetching Data...
+export default class LoadingScreen extends React.Component<Props, State> {
+  constructor() {
+    super()
+    this.state = {
+      isLoadingComplete: false
+    }
+    _getPreferences()
+  }
+
+  render(): JSX.Element {
+    return (
+      <AppLoading
+        startAsync={this._loadResourcesAsync}
+        onError={this._handleLoadingError}
+        onFinish={this._handleFinishLoading}
+      />
+    )
+  }
+
+ _loadResourcesAsync: void = async () => {
+   await Promise.all([
+     Asset.loadAsync([
+       require('../../assets/logo/Ebay(300-120)')
+     ]),
+     Font.loadAsync({
+       robotoRegular: require('../../../src/assets/font/Roboto-Regular.ttf'),
+       robotoCondensed: require('../../../src/assets/font/robotoCondensed.ttf'),
+       impact: require('../../../src/assets/font/impact.ttf'),
+       latoLightitalic: require('../../../src/assets/font/Lato-LightItalic.ttf'),
+       latoHairline: require('../../../src/assets/font/Lato-Hairline.ttf')
+     })
+   ])
+ }
+
+ _handleLoadingError: void = (error: Error) => {
+   console.log(error)
+ }
+
+ _handleFinishLoading: void = () => {
+   this.setState({ isLoadingComplete: true })
+   this.props.navigation.navigate('App')
+ }
+}

881cb6e903362aa4353764189178062e624aae82
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:24:23 +0100

    [added] baseScreens for logic  testing
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/screens/home/main.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+class HomeScreen extends Component {
+  render(): JSX.Element {
+    return (
+      <View>
+        <Text> H o m e </Text>
+      </View>
+    )
+  }
+}
+
+export default HomeScreen
---
+++ src/screens/settings/main.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+class SettingsScreen extends Component {
+  render(): JSX.Element {
+    return (
+      <View>
+        <Text> S E T T I N G S </Text>
+      </View>
+    )
+  }
+}
+
+export default SettingsScreen

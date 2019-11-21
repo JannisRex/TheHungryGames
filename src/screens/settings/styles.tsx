6b54b5f1b28d0a972054d5c6869e10be68426e11
Jannis Rex <rex.jannis@web.de>
2019-11-21 13:12:28 +0100

    [added] screen directory
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/screens/settings/styles.tsx
@@ -0,0 +1,4 @@
+// used Screens
+export { default as HomeScreen } from '../screens/home/main'
+export { default as InitialScreen } from '../screens/initialLoad/main'
+export { default as SettingsScreen } from '../screens/settings/main'

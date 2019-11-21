4e7e7b7faaf1c2eaa19893c0ff897ed3e38b728b
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:27:45 +0100

    [added]  basic styleSheets for both screens
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/screens/home/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- src/screens/settings/styles.tsx
+++ src/screens/settings/styles.tsx
@@ -1,4 +1,10 @@
-// used Screens
-export { default as HomeScreen } from '../screens/home/main'
-export { default as InitialScreen } from '../screens/initialLoad/main'
-export { default as SettingsScreen } from '../screens/settings/main'
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})

53ee46eace6b5660a481be60e804a6bb53010b16
Jannis Rex <rex.jannis@web.de>
2019-11-21 13:12:32 +0100

    [added] default values to use (fontSize, colors etc.)
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/config/theme.style.tsx
@@ -0,0 +1,42 @@
+// @flow strict
+import { Platform } from 'react-native'
+const isIOS = Platform.OS === 'ios'
+
+export default {
+  // Basic Font Options
+  COLOR_TEXT: 'rgb(255, 255, 240)',
+  FONT_FAMILY_TITLE: 'impact',
+  FONT_FAMILY_CAPTION: 'lato_lightitalic',
+  FONT_FAMILY_TEXT_ANDROID: 'Roboto',
+  FONT_FAMILY_TEXT_IOS: 'San Francisco',
+  // FONT_FAMILY_TEXT: isIOS ? 'San Francisco' : 'Roboto',
+  FONT_FAMILY_TEXT: isIOS ? 'robotoCondensed' : 'Roboto',
+
+  FONT_SIZE_TINY: 15,
+  FONT_SIZE_SMALL: 20,
+  FONT_SIZE_MEDIUM: 26,
+  FONT_SIZE_LARGE: 35,
+  FONT_SIZE_LARGER: 42,
+  FONT_SIZE_TESTING: 55,
+  FONT_WEIGHT_THIN: '100',
+  FONT_WEIGHT_LIGHT: '300',
+  FONT_WEIGHT_MEDIUM: '600',
+  FONT_WEIGHT_HEAVY: '800',
+
+  // Color Schemes (fix Primary, Secondary etc. to switch later)
+  COLOR_WHITE: 'rgb(255, 255, 255)',
+  COLOR_IVORY: 'rgb(255, 255, 240)',
+  COLOR_HONEYDEW: 'rgb(240, 255, 240)',
+  COLOR_LIGHTGRAY: 'rgb(159,161,163)',
+  COLOR_DARKGRAY: 'rgb(55, 58, 60)',
+  COLOR_DARKERGRAY: 'rgb(35, 38, 40)',
+  COLOR_BUTTON_DARK: '#2E1114',
+  COLOR_TINT: '#2f95dc',
+
+  // generic Warning/Error message colors
+  WARNING_BACKGROUND: '#EAEB5E',
+  WARNING_TEXT: '#666804',
+  ERROR_BACKGROUND: '#45b3e0',
+  ERROR_TEXT: '#2E1114',
+  ERROR_HEADER: '#1b0a0c'
+}

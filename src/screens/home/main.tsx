56dd93bc4a85fc1eed046369cb7165cc2df7a87b
Jannis Rex <rex.jannis@web.de>
2019-11-21 14:30:43 +0100

    [eslint] prettified
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import styles from "./styles"
+import styles from './styles'
 
 class HomeScreen extends Component {
   render(): JSX.Element {

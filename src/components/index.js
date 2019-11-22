c5771590f8113056aebfacbcb85db0f5633ed82d
Jannis Rex <rex.jannis@web.de>
2019-11-22 13:59:33 +0100

    [renamed]
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Container } from './Container/main'
-export { default as ErrorBoundary } from './ErrorBoundary/main'
-export { default as ErrSubstituteScreen } from './ErrSubstituteScreen/main'
-

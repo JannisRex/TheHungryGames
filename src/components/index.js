7954946d961c4fe182fa9ccb3d5daa608365466c
Jannis Rex <rex.jannis@web.de>
2019-11-22 13:38:02 +0100

    [added] Boundary & SubScreen to exports
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Container } from './Container/main'
+export { default as ErrorBoundary } from './ErrorBoundary/main'
+export { default as ErrSubstituteScreen } from './ErrSubstituteScreen/main'
 

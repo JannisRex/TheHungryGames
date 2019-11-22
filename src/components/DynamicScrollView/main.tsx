04353518b49acbf98b8f8bdc7f9564fad730c3f8
Jannis Rex <rex.jannis@web.de>
2019-11-22 13:56:23 +0100

    [added] Component to have a dynamic scrollview, lol who whould have guessed :X
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/components/DynamicScrollView/main.tsx
@@ -0,0 +1,49 @@
+// @flow strict
+import React, { Component } from 'react'
+import { SafeAreaView, StatusBar, View, ScrollView, Dimensions } from 'react-native'
+import styles from './styles'
+
+const { height } = Dimensions.get('window')
+
+type Props = {
+  children: Node,
+  color: string,
+}
+
+type State = {
+  screenHeight: number
+}
+
+export default class DynamicScrollView extends Component<Props, State> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      screenHeight: height
+    }
+  }
+
+  _handleContentSizeChange = (contentWidth, contentHeight) => {
+    this.setState({ screenHeight: contentHeight })
+  }
+
+  render(): JSX.Element {
+    const color = this.props.color
+    const scrollEnabled = this.state.screenHeight > height
+    return (
+      <SafeAreaView style={[styles.container, { backgroundColor: color }]}>
+        <StatusBar barStyle='light-content' backgroundColor='#468189' />
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={styles.scrollview}
+          scrollEnabled={scrollEnabled}
+          onContentSizeChange={this._handleContentSizeChange}
+        >
+          <View style={styles.content}>
+            {this.props.children}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    )
+  }
+}

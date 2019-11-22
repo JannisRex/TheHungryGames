abe7bfbcece7ecae99936d1413a94d8d70c30a12
Jannis Rex <rex.jannis@web.de>
2019-11-22 14:06:00 +0100

    [added] styling for errors
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

---
+++ src/components/ErrSubstituteScreen/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet, ViewStyle, TextStyle } from 'react-native'
+import theme from '../../config/theme.style'
+
+type Styles = {
+  button: ViewStyle,
+  errorHeader: TextStyle,
+  errorInfo: ViewStyle,
+  errorContainer: ViewStyle
+}
+
+export default StyleSheet.create<Styles>({
+  button: {
+    backgroundColor: theme.COLOR_BUTTON_DARK,
+    borderRadius: 50,
+    padding: 15
+  },
+errorHeader: {
+    fontFamily: 'lato_lightitalic',
+    fontSize: theme.FONT_SIZE_LARGE,
+    fontStyle: 'normal',
+    fontWeight: theme.FONT_WEIGHT_THIN,
+    color: theme.ERROR_HEADER,
+    textAlign: 'center'
+  },
+  errorInfo: {
+    fontFamily: 'lato_hairline',
+    fontSize: theme.FONT_SIZE_SMALL,
+    fontStyle: 'normal',
+    fontWeight: theme.FONT_WEIGHT_MEDIUM,
+    color: theme.ERROR_TEXT,
+    paddingVertical: 10
+  },
+  errorContainer: {
+    flex: 1,
+    marginHorizontal: 12,
+    backgroundColor: theme.ERROR_BACKGROUND
+  }
+})

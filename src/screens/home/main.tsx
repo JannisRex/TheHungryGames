d32b7ba09a8d6c9673fa2c4cde3c648c6e8ba77f
Jannis Rex <rex.jannis@web.de>
2019-11-25 11:22:47 +0100

    [added] basic layout for screen
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -4,14 +4,19 @@ import { Container, Header } from '../../components/index'
 import strings from '../../config/strings'
 import styles from './styles'
 
+import flex from '../../lib/flexboxHelper'
+
 class HomeScreen extends Component {
   render(): JSX.Element {
     return (
       <Container>
         <Header />
         <View style={styles.container}>
-          <Text>{strings.HomeScreen.title}</Text>
-          <Text>Button: {strings.HomeScreen.pressButton}</Text>
+          <View style={flex.a2}><Text>1</Text></View>
+          <View style={flex.a1}><Text>2</Text></View>
+          <View style={flex.a3}><Text>3</Text></View>
+          {/* <Text>{strings.HomeScreen.title}</Text> */}
+          {/* <Text>Button: {strings.HomeScreen.pressButton}</Text> */}
         </View>
       </Container>
     )

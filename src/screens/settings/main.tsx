39f42bc3a0c0a3d5e97a747f8491e7a335c8ac73
Jannis Rex <rex.jannis@web.de>
2019-11-25 11:31:02 +0100

    [added] colored views to think about flex box layouting
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/settings/main.tsx
+++ src/screens/settings/main.tsx
@@ -4,13 +4,35 @@ import { Container, Header } from '../../components/index'
 import strings from '../../config/strings'
 import styles from './styles'
 
+import flex from '../../lib/flexboxHelper'
+
 class SettingsScreen extends Component {
   render(): JSX.Element {
     return (
       <Container>
         <Header />
         <View style={styles.container}>
-          <Text>{strings.SettingsScreen.title}</Text>
+          <View style={flex.a4}>
+            <Text>++ Input: PLZ => City (get from zipCodeList) ++</Text>
+            <Text>++ Slider: RangeMax ++</Text>
+          </View>
+
+          <View style={flex.a4}>
+            <Text>++ CheckBoxes: Food Type ++</Text>
+            <Text>[ ] [ ] [X] [X] [ ]</Text>
+          </View>
+
+          <View style={flex.a2}>
+            <Text>++ Toggle: Yes/No ++</Text>
+            <Text>++ ButtonGroup: [$], [$$], [$$$] ++</Text>
+          </View>
+
+          <View style={flex.a3}>
+            <Text>extras???</Text>
+            <Text>Checkboxes: e.g. canOrder alcohol...</Text>
+          </View>
+
+          {/* <Text>{strings.SettingsScreen.title}</Text> */}
         </View>
       </Container>
     )

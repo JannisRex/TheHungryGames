7dd5ce9566fc24532534637af39fc9a11a45b7de
Jannis Rex <rex.jannis@web.de>
2019-11-25 10:01:09 +0100

    [added] <Container /> && <Header />
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { Container, Header } from '../../components/index'
 import strings from '../../config/strings'
 import styles from './styles'
 
 class HomeScreen extends Component {
   render(): JSX.Element {
     return (
-      <View style={styles.container}>
-        <Text>{strings.HomeScreen.title}</Text>
-        <Text>Button: {strings.HomeScreen.pressButton}</Text>
-      </View>
+      <Container>
+        <Header />
+        <View style={styles.container}>
+          <Text>{strings.HomeScreen.title}</Text>
+          <Text>Button: {strings.HomeScreen.pressButton}</Text>
+        </View>
+      </Container>
     )
   }
 }

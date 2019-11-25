21ba70d19ace50b021b747ad843d680573c0666b
Jannis Rex <rex.jannis@web.de>
2019-11-25 09:47:31 +0100

    [added] strings from config
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import strings from '../../config/strings'
 import styles from './styles'
 
 class HomeScreen extends Component {
   render(): JSX.Element {
     return (
       <View style={styles.container}>
-        <Text>H O M E</Text>
+        <Text>{strings.HomeScreen.title}</Text>
+        <Text>Button: {strings.HomeScreen.pressButton}</Text>
       </View>
     )
   }

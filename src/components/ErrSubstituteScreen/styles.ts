b8f4c514771e22ae4b3d109c8e6f44bea636c537
Jannis Rex <rex.jannis@web.de>
2019-11-25 09:04:48 +0100

    [TS] casted StyleTypes to be able to use theme
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/components/ErrSubstituteScreen/styles.ts
+++ src/components/ErrSubstituteScreen/styles.ts
@@ -4,33 +4,35 @@ import theme from '../../config/theme.style'
 type Styles = {
   button: ViewStyle,
   errorHeader: TextStyle,
-  errorInfo: ViewStyle,
+  errorInfo: TextStyle,
   errorContainer: ViewStyle
 }
 
-// TODO fix types
+// TODO fix variable string err
+// when using theme = '200' e.g. => error
+// fontWeight: string = '200' => O.K.
 export default StyleSheet.create<Styles>({
   button: {
     backgroundColor: theme.COLOR_BUTTON_DARK,
     borderRadius: 50,
     padding: 15
   },
-errorHeader: {
+  errorHeader: {
     fontFamily: 'lato_lightitalic',
     fontSize: theme.FONT_SIZE_LARGE,
-    fontStyle: 'normal',
+    // fontStyle: 'normal',
     fontWeight: theme.FONT_WEIGHT_THIN,
     color: theme.ERROR_HEADER,
     textAlign: 'center'
-  },
+  } as TextStyle,
   errorInfo: {
     fontFamily: 'lato_hairline',
     fontSize: theme.FONT_SIZE_SMALL,
-    fontStyle: 'normal',
+    // fontStyle: 'normal',
     fontWeight: theme.FONT_WEIGHT_MEDIUM,
     color: theme.ERROR_TEXT,
     paddingVertical: 10
-  },
+  } as TextStyle,
   errorContainer: {
     flex: 1,
     marginHorizontal: 12,

8fcb3d0b9bf4e97887cb0622abd36ecdb98e7839
Jannis Rex <rex.jannis@web.de>
2019-11-25 11:22:44 +0100

    [added] jC and aI to flexbox aswell as width 100%
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/lib/flexboxHelper.ts
+++ src/lib/flexboxHelper.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, ViewStyle } from 'react-native'
+import { Dimensions, StyleSheet, ViewStyle } from 'react-native'
 
 type Styles = {
   a1: ViewStyle,
@@ -7,21 +7,35 @@ type Styles = {
   a4: ViewStyle
 }
 
+const { width } = Dimensions.get('window')
+
 export default StyleSheet.create<Styles>({
   a1: {
-    flex: 1,
-    backgroundColor: 'red'
+    flex: 3,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width
   },
   a2: {
     flex: 2,
-    backgroundColor: 'blue'
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width
   },
   a3: {
-    flex: 3,
-    backgroundColor: 'green'
+    flex: 1.5,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width
   },
   a4: {
-    flex: 4,
-    backgroundColor: 'yellow'
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width
   }
 })

7721b598690523e2a1b6e161e85d97638d20e532
Jannis Rex <rex.jannis@web.de>
2019-11-27 14:47:35 +0100

    [added] Slider && InputField in first flex
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/settings/main.tsx
+++ src/screens/settings/main.tsx
@@ -1,39 +1,62 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Switch } from 'react-native'
 import { Container, Header } from '../../components/index'
+import { Input, Slider, CheckBox, ButtonGroup } from 'react-native-elements'
 import strings from '../../config/strings'
 import styles from './styles'
 
 import flex from '../../lib/flexboxHelper'
 
-class SettingsScreen extends Component {
+type Props = {}
+
+type State = {
+  sliderValue: number
+}
+
+class SettingsScreen extends Component<Props, State> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      sliderValue: 0
+    }
+  }
+
   render(): JSX.Element {
     return (
       <Container>
         <Header />
-        <View style={styles.container}>
-          <View style={flex.a4}>
-            <Text>++ Input: PLZ => City (get from zipCodeList) ++</Text>
-            <Text>++ Slider: RangeMax ++</Text>
+        <View style={flex.a4}>
+          <View style={{ flex: 1 }}>
+            <Input
+              placeholder='Enter Zip-Code'
+            />
           </View>
 
-          <View style={flex.a4}>
-            <Text>++ CheckBoxes: Food Type ++</Text>
-            <Text>[ ] [ ] [X] [X] [ ]</Text>
+          <View style={{ flex: 1 }}>
+            <Slider
+              value={this.state.sliderValue}
+              onValueChange={(value: number): void => this.setState({ sliderValue: value })}
+            />
           </View>
-
-          <View style={flex.a2}>
-            <Text>++ Toggle: Yes/No ++</Text>
-            <Text>++ ButtonGroup: [$], [$$], [$$$] ++</Text>
-          </View>
-
-          <View style={flex.a3}>
-            <Text>extras???</Text>
-            <Text>Checkboxes: e.g. canOrder alcohol...</Text>
-          </View>
-
-          {/* <Text>{strings.SettingsScreen.title}</Text> */}
         </View>
+
+        <View style={flex.a4}>
+          <Text>++ CheckBoxes: Food Type ++</Text>
+          <Text>[ ] [ ] [X] [X] [ ]</Text>
+        </View>
+
+        <View style={flex.a2}>
+          <Text>++ Toggle: Yes/No ++</Text>
+          <Text>++ ButtonGroup: [$], [$$], [$$$] ++</Text>
+        </View>
+
+        <View style={flex.a3}>
+          <Text>extras???</Text>
+          <Text>Checkboxes: e.g. canOrder alcohol...</Text>
+        </View>
+
+        {/* <Text>{strings.SettingsScreen.title}</Text> */}
       </Container>
     )
   }

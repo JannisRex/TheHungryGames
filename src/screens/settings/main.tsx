1598881a4742a93943a0a619c802b313cff4eacc
Jannis Rex <rex.jannis@web.de>
2019-11-27 16:12:57 +0100

    [eslint] prettified"
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/settings/main.tsx
+++ src/screens/settings/main.tsx
@@ -61,82 +61,82 @@ readonly _updateIndex: any = (newIndex: number): void => {
   })
 }
 
-  render(): JSX.Element {
-    return (
-      <Container>
-        <Header />
+render(): JSX.Element {
+  return (
+    <Container>
+      <Header />
 
-        <View style={flex.a4}>
-          {/* Upper Segment - evaluates ZIP-Code */}
-          {/* and gets according CityName */}
-          <View style={{ flex: 1 }}>
-            <Input
-              placeholder='Enter Zip-Code'
-            />
-          </View>
-
-          {/* to set max Range a restaurant */}
-          {/* is allowed to have as distance */}
-          <View style={{ flex: 1 }}>
-            <Slider
-              value={this.state.sliderValue}
-              onValueChange={(value: number): void => this.setState({ sliderValue: value })}
-            />
-          </View>
+      <View style={flex.a4}>
+        {/* Upper Segment - evaluates ZIP-Code */}
+        {/* and gets according CityName */}
+        <View style={{ flex: 1 }}>
+          <Input
+            placeholder='Enter Zip-Code'
+          />
         </View>
 
-        <View style={flex.a4}>
-          {/* essentially just checkBoxes in rows, */}
-          {/* gets layouted by flexBoxes */}
-          <View style={{ flex: 1, flexDirection: 'column' }}>
-            {/* First Row */}
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-              <CheckBox title='cB1' checked={this.state.checkBoxesFood.cB1}/>
-              <CheckBox title='cB2' checked={this.state.checkBoxesFood.cB2}/>
-              <CheckBox title='cB3' checked={this.state.checkBoxesFood.cB3}/>
-              <CheckBox title='cB4' checked={this.state.checkBoxesFood.cB4}/>
-            </View>
-            {/* Second Row */}
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-              <CheckBox title='cB5' checked={this.state.checkBoxesFood.cB5}/>
-              <CheckBox title='cB6' checked={this.state.checkBoxesFood.cB6}/>
-              <CheckBox title='cB7' checked={this.state.checkBoxesFood.cB7}/>
-              <CheckBox title='cB8' checked={this.state.checkBoxesFood.cB8}/>
-            </View>
-            {/* Third Row */}
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-              <CheckBox title='cB9' checked={this.state.checkBoxesFood.cB9}/>
-              <CheckBox title='cB10' checked={this.state.checkBoxesFood.cB10}/>
-              <CheckBox title='cB11' checked={this.state.checkBoxesFood.cB11}/>
-              <CheckBox title='cB12' checked={this.state.checkBoxesFood.cB12}/>
-            </View>
+        {/* to set max Range a restaurant */}
+        {/* is allowed to have as distance */}
+        <View style={{ flex: 1 }}>
+          <Slider
+            value={this.state.sliderValue}
+            onValueChange={(value: number): void => this.setState({ sliderValue: value })}
+          />
+        </View>
+      </View>
+
+      <View style={flex.a4}>
+        {/* essentially just checkBoxes in rows, */}
+        {/* gets layouted by flexBoxes */}
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          {/* First Row */}
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <CheckBox title='cB1' checked={this.state.checkBoxesFood.cB1}/>
+            <CheckBox title='cB2' checked={this.state.checkBoxesFood.cB2}/>
+            <CheckBox title='cB3' checked={this.state.checkBoxesFood.cB3}/>
+            <CheckBox title='cB4' checked={this.state.checkBoxesFood.cB4}/>
+          </View>
+          {/* Second Row */}
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <CheckBox title='cB5' checked={this.state.checkBoxesFood.cB5}/>
+            <CheckBox title='cB6' checked={this.state.checkBoxesFood.cB6}/>
+            <CheckBox title='cB7' checked={this.state.checkBoxesFood.cB7}/>
+            <CheckBox title='cB8' checked={this.state.checkBoxesFood.cB8}/>
+          </View>
+          {/* Third Row */}
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <CheckBox title='cB9' checked={this.state.checkBoxesFood.cB9}/>
+            <CheckBox title='cB10' checked={this.state.checkBoxesFood.cB10}/>
+            <CheckBox title='cB11' checked={this.state.checkBoxesFood.cB11}/>
+            <CheckBox title='cB12' checked={this.state.checkBoxesFood.cB12}/>
           </View>
         </View>
+      </View>
 
-        <View style={flex.a2}>
-          <View style={{ flex: 1 }}>
-            <Text>++ Toggle: Yes/No ++</Text>
-            <ButtonGroup
-      onPress={this._updateIndex}
-      selectedIndex={this.state.selectedIndex}
-      buttons={['$', '$$', '$$$']}
-      containerStyle={{ height: 100 }}
-/>
-            <Text>++ ButtonGroup: [$], [$$], [$$$] ++</Text>
-          </View>
+      <View style={flex.a2}>
+        <View style={{ flex: 1 }}>
+          <Text>++ Toggle: Yes/No ++</Text>
+          <ButtonGroup
+            onPress={this._updateIndex}
+            selectedIndex={this.state.selectedIndex}
+            buttons={['$', '$$', '$$$']}
+            containerStyle={{ height: 100 }}
+          />
+          <Text>++ ButtonGroup: [$], [$$], [$$$] ++</Text>
         </View>
+      </View>
 
-        <View style={flex.a3}>
-          <View style={{ flex: 1 }}>
-            <Text>extras???</Text>
-            <Text>Checkboxes: e.g. canOrder alcohol...</Text>
-          </View>
+      <View style={flex.a3}>
+        <View style={{ flex: 1 }}>
+          <Text>extras???</Text>
+          <Text>Checkboxes: e.g. canOrder alcohol...</Text>
         </View>
+      </View>
 
-        {/* <Text>{strings.SettingsScreen.title}</Text> */}
-      </Container>
-    )
-  }
+      {/* <Text>{strings.SettingsScreen.title}</Text> */}
+    </Container>
+  )
+}
 }
 
 export default SettingsScreen

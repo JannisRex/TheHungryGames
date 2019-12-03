e317136ae69bd5c469a41849828400788417f463
Jannis Rex <rex.jannis@web.de>
2019-12-03 11:34:13 +0100

    [added] textShadow for text, title, flavor
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -3,6 +3,7 @@ import theme from '../../config/theme.style'
 
 type Styles = {
     container: ViewStyle,
+    text: TextStyle,
     textTitle: TextStyle,
     textFlavor: TextStyle,
     textRestaurant: TextStyle
@@ -14,22 +15,37 @@ export default StyleSheet.create<Styles>({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  text: {
+    fontSize: theme.FONT_SIZE_SMALL,
+    fontFamily: theme.FONT_FAMILY_TEXT,
+    fontWeight: theme.FONT_WEIGHT_MEDIUM,
+    color: theme.COLOR_HONEYDEW
+  } as TextStyle,
   textTitle: {
     fontSize: theme.FONT_SIZE_LARGE,
     fontFamily: theme.FONT_FAMILY_TITLE,
     fontWeight: theme.FONT_WEIGHT_LIGHT,
-    color: theme.COLOR_IVORY
+    color: theme.COLOR_IVORY,
+    textShadowColor: '#000',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
   } as TextStyle,
   textFlavor: {
     fontSize: theme.FONT_SIZE_MEDIUM,
     fontFamily: theme.FONT_FAMILY_CAPTION,
-    fontWeight: theme.FONT_WEIGHT_MEDIUM,
-    color: theme.COLOR_IVORY
+    fontWeight: theme.FONT_WEIGHT_LIGHT,
+    color: theme.COLOR_IVORY,
+    textShadowColor: '#000',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 5
   } as TextStyle,
   textRestaurant: {
     fontSize: theme.FONT_SIZE_MEDIUM,
     fontFamily: theme.FONT_FAMILY_THIN,
     fontWeight: theme.FONT_WEIGHT_HEAVY,
-    color: theme.COLOR_IVORY
+    color: theme.COLOR_IVORY,
+    textShadowColor: '#000',
+    textShadowOffset: { width: 0.5, height: -0.5 },
+    textShadowRadius: 15
   } as TextStyle
 })

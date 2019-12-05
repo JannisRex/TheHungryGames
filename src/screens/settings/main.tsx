e7169db96ee6111537a8fc2603bf74214eb1e040
Jannis Rex <rex.jannis@web.de>
2019-12-05 08:45:16 +0100

    [changed] var name to item
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/settings/main.tsx
+++ src/screens/settings/main.tsx
@@ -72,9 +72,9 @@ class SettingsScreen extends Component<Props, State> {
     try {
       await AsyncStorage.getAllKeys().then(async (keys: string[]) => {
         const result = await AsyncStorage.multiGet(keys)
-        return result.map((req: string[]) => {
-          console.log('0: ', JSON.parse(req[0]))
-          console.log('1: ', JSON.parse(req[1]))
+        return result.map((item: string[]) => {
+          console.log('0: ', JSON.parse(item[0]))
+          console.log('1: ', JSON.parse(item[1]))
         })
       })
     } catch (e) {

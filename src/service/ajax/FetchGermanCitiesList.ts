46092593fd520d67bd58cd7834dd19c136644f87
Jannis Rex <rex.jannis@web.de>
2019-12-09 08:42:16 +0100

    [TS] typed && [added] basic fetch, which returns Response Promise, which then can be used to get the data
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/service/ajax/FetchGermanCitiesList.ts
+++ src/service/ajax/FetchGermanCitiesList.ts
@@ -1,1 +1,6 @@
-// https://gist.githubusercontent.com/JannisRex/fdec2cb0623e0426b299bf0bb02c8d52/raw/7c98d2379a30e55e2e854429f5c46c1732046cd2/German-Zip-Codes.json
+import { zipUrl } from '../../config/constants'
+
+export const FetchGermanCitiesList: any = async (): Promise<Response> => {
+  return fetch(zipUrl)
+    .then(async (response: Response) => response.json())
+}

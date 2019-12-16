00299f9b52f8398c52f5ebfc2c250e0cbf8cd95c
Jannis Rex <rex.jannis@web.de>
2019-12-16 08:32:40 +0100

    [added] stringify() around array<objects passed from initialLaod
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -71,7 +71,7 @@ class HomeScreen extends Component<Props, State> {
   componentDidMount(): void {
     const e = this.props.navigation.getParam('zipCodeData', null)
     console.log('Type: ', typeof e)
-    console.log('First Entry: ', e[0])
+    console.log('First Entry: ', JSON.stringify(e[0]))
   // AsyncStorage Logic Here
   // Check if Settings are fetchable
   // Fetch Or set defaultValues to State
